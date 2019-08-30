2d862ae1be74635776ef2eec0f955464d8aa829d
Keiichiro Seto <shostakovich24@gmail.com>
2019-08-31 03:47:56 +0900

    sortして遊ぶことはできる

---
+++ src/views/Room.vue
@@ -0,0 +1,57 @@
+<template>
+  <draggable
+    v-model="myArray"
+    group="people"
+    @start="drag = true"
+    @end="drag = false"
+  >
+    <div class="element" v-for="element in myArray" :key="element.id">
+      <p>{{ element.name }}</p>
+    </div>
+  </draggable>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  name: "home",
+  components: {
+    draggable
+  },
+  data: function() {
+    return {
+      myArray: [
+        {
+          id: "aiu",
+          name: "aiuf"
+        },
+        {
+          id: "eaiu",
+          name: "faenklajf"
+        }
+      ]
+    };
+  },
+  created: function() {
+    for (let index = 0; index < 10; index++) {
+      this.myArray.push({ id: 12312 + index, name: 121 * index });
+    }
+    for (let i = this.myArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var tmp = this.myArray[i];
+      this.myArray[i] = this.myArray[j];
+      this.myArray[j] = tmp;
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.element
+  border: 1px solid black
+  p
+    vertical-align: middle
+.sortable-chosen
+  background-color: red
+</style>
